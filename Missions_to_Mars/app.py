3903f28438dff5ec39ce3c8693822e886c730c6c
efennie <efennie@email.arizona.edu>
2021-06-12 19:56:15 -0700

    sorting out the app

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scraper
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:5000/scrape_mars"
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def index():
+    mars_info = mongo.db.mars_info.find_one()
+    return render_template("index.html", mars_info=mars_info)
+
+
+@app.route("/scrape")
+def scraper():
+    data = mongo.db.mars_info
+    mars_data = scraper.scrape()
+    data.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
