a274e505cfb766ab3980c96b83248cb66a274175
efennie <efennie@email.arizona.edu>
2021-06-12 21:23:57 -0700

    all sorts of edits trying to connect db

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -5,22 +5,22 @@ import scraper
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:5000/scrape_mars"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/scraper"
 mongo = PyMongo(app)
 
 
 @app.route("/")
 def index():
-    mars_info = mongo.db.mars_info.find_one()
-    return render_template("index.html", mars_info=mars_info)
+    mars_info = mongo.db.mars.find_one()
+    return render_template("index.html", mars = mars_info)
 
 
 @app.route("/scrape")
-def scraper():
-    data = mongo.db.mars_info
+def scraped():
+    data = mongo.db.mars
     scraped_data = scraper.scrape()
     data.update({}, scraped_data, upsert=True)
-    return redirect("/", code=302)
+    return redirect("http://localhost:5000/", code=302)
 
 
 if __name__ == "__main__":
